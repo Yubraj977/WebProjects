22881b1e4f167848cb2684fa40926b92b3b16fad
yubraj977 <yubrajkhatri977@gmail.com>
2023-11-06 12:07:24 +0545

    this is the new project where i add the digital clock

---
+++ digital clock/main.js
@@ -0,0 +1,27 @@
+const changebtn = document.getElementsByClassName("switch")[0]; 
+const main = document.getElementsByClassName("main")[0]; 
+
+
+changebtn.addEventListener('click', function () {
+  
+  main.classList.toggle("mode");
+});
+
+
+
+
+function updateClock(){
+  const updateElement=document.getElementsByClassName("time")[0]
+  const updatemilli=document.getElementsByClassName("millisec")[0]
+  const now=new Date();
+  const hours=String(now.getHours()).padStart(2,"0")
+  const minutes=String(now.getMinutes()).padStart(2,"0")
+  const seconds=String(now.getSeconds()).padStart(2,"0")
+  const millisec=String(now.getMilliseconds()).padStart(3,"0")
+ 
+  timestring=`${hours}:${minutes}:${seconds}`
+  updateElement.textContent=timestring
+}
+updateClock();
+setInterval(updateClock,200)
+
